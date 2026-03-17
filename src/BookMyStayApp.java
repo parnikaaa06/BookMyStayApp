59f1de917cf17da41cfe9ef2dd3e3693c7022143
Parnikaaa06 <puranikparnika06@gmail.com>
2026-03-17 15:14:34 +0530

    UC2:Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,6 +1,82 @@
+import java.util.Scanner;
+
 public class BookMyStayApp {
-    public static void main (String[] args){
-        System.out.println("Welcome to Hotel Booking Management System");
+
+    protected String roomNumber;
+    protected int numberOfBeds;
+    protected double roomSize;
+    protected double pricePerNight;
+    protected String roomType;
+
+    // Constructor
+    public BookMyStayApp(String roomNumber, int numberOfBeds, double roomSize, double pricePerNight, String roomType) {
+        this.roomNumber = roomNumber;
+        this.numberOfBeds = numberOfBeds;
+        this.roomSize = roomSize;
+        this.pricePerNight = pricePerNight;
+        this.roomType = roomType;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public double getRoomSize() {
+        return roomSize;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("\nRoom Details");
+        System.out.println("Room Number   : " + roomNumber);
+        System.out.println("Room Type     : " + roomType);
+        System.out.println("Number of Beds: " + numberOfBeds);
+        System.out.println("Room Size     : " + roomSize + " sq ft");
+        System.out.println("Price/Night   : $" + pricePerNight);
+        System.out.println("-----------------------------");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println(" Welcome to Hotel Booking System");
         System.out.println("System initialized successfully.");
+
+        System.out.print("Enter Room Number: ");
+        String roomNumber = sc.nextLine();
+
+        System.out.print("Enter Room Type (Single/Double/Suite): ");
+        String roomType = sc.nextLine();
+
+        System.out.print("Enter Number of Beds: ");
+        int beds = sc.nextInt();
+
+        System.out.print("Enter Room Size (sq ft): ");
+        double size = sc.nextDouble();
+
+        System.out.print("Enter Price per Night: ");
+        double price = sc.nextDouble();
+
+        BookMyStayApp room = new BookMyStayApp(roomNumber, beds, size, price, roomType);
+
+        System.out.println("\nApplication started successfully.");
+        System.out.println("System is ready to manage hotel bookings.");
+
+        room.displayRoomDetails();
+
+        sc.close();
     }
 }
